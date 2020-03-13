673615b6c657373472a5c07d20b4cc585fe00141
gabriel-lgomes <gabriel_lopes.g@hotmail.com>
2020-03-13 16:21:51 -0300

    filtro e video

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 ?>
 
 
-    <section class="visite"> 
+    <section class="visite" id="visite"> 
         <div class="container">
             <!-- Title -->
             <div class="row">
@@ -18,46 +18,287 @@
                 </div>
             </div>
             <!-- Buttons Filter -->
-            <div class="row">
-                <div class="col-md-2 col-2">
-                    <a href="#" class="btn btn-leste">
+            <div class="row" id="filter">
+                <div class="col-md-2 col-6 m-10">
+                    <a href="#" class="btn btn-leste" id="zona-leste">
                         ZONA LESTE
                     </a>
                 </div>
-                <div class="col-md-2 col-2">
-                    <a href="#" class="btn btn-sul">
+                <div class="col-md-2 col-6 m-10" >
+                    <a href="#" class="btn btn-sul" id="zona-sul">
                         ZONA SUL
                     </a>
                 </div>
-                <div class="col-md-2 col-2">
-                    <a href="#" class="btn btn-norte">
+                <div class="col-md-2 col-6 m-10" >
+                    <a href="#" class="btn btn-norte" id="zona-norte">
                         ZONA NORTE
                     </a>
                 </div>
-                <div class="col-md-2 col-2">
-                    <a href="#" class="btn btn-oeste">
+                <div class="col-md-2 col-6 m-10" >
+                    <a href="#" class="btn btn-oeste" id="zona-oeste">
                         ZONA OESTE
                     </a>
                 </div>
-                <div class="col-md-2 col-2">
-                    <a href="#" class="btn btn-centro">
+                <div class="col-md-2 col-6 m-10">
+                    <a href="#" class="btn btn-centro" id="centro">
                         CENTRO
                     </a>
                 </div>
-                <div class="col-md-2 col-2">
-                    <a href="#" class="btn btn-jundiai">
+                <div class="col-md-2 col-6 m-10" >
+                    <a href="#" class="btn btn-jundiai" id="jundiai">
                         JUNDIAÍ
                     </a>
                 </div>
             </div>
             <!-- Grid Region -->
-            <div class="row">
-                <div class="col-md-3 col-12">
-                    
+            <div class="row m-50">
+                <!-- Partial 1 -->
+                <div class="col-md-3 col-12 m-20 p-0 todos zona-sul">
+                    <div class="visite--region">
+                        <p class="title">
+                            Sacomã
+                        </p>
+                        <p class="text h-65">
+                            Rua Dom Vilares, 1227 - Vila das Mercês
+                        </p>
+                        <p class="text m-30">
+                            <strong>Horário de funcionamento: 09h00 às 18h00</strong>
+                        </p>
+                        <a href="#" target="_blank" class="btn btn-red">Como chegar</a>
+                    </div>
                 </div>
-                <div class="col-md-3 col-12"></div>
-                <div class="col-md-3 col-12"></div>
-                <div class="col-md-3 col-12"></div>
+                <div class="col-md-3 col-12 m-20 p-0 todos zona-oeste">
+                    <div class="visite--region">
+                        <p class="title">
+                            Zona Oeste
+                        </p>
+                        <p class="text h-65">
+                            Rua Caminho do Engenho, 600 (esquina c/ Avenida Pirajussara)
+                        </p>
+                        <p class="text m-30">
+                            <strong>Horário de funcionamento: 09h00 às 20h00</strong>
+                        </p>
+                        <a href="#" target="_blank" class="btn btn-red">Como chegar</a>
+                    </div>
+                </div>
+                <div class="col-md-3 col-12 m-20 p-0 todos zona-leste">
+                    <div class="visite--region"> 
+                        <p class="title">
+                            Penha
+                        </p>
+                        <p class="text h-65">
+                            Avenida São Miguel, 962 (estacionamento do Extra Hipermercado)
+                        </p>
+                        <p class="text m-30">
+                            <strong>Horário de funcionamento: 08h00 às 20h00</strong>
+                        </p>
+                        <a href="#" target="_blank" class="btn btn-red">Como chegar</a> 
+                    </div>
+                </div>
+                <div class="col-md-3 col-12 m-20 p-0 todos zona-leste">
+                     <div class="visite--region"> 
+                        <p class="title">
+                            Anhaia Melo
+                        </p>
+                        <p class="text h-65">
+                            Avenida Prof. Luiz Inácio de Anhaia Melo, 2.785 – Vila Prudente
+                        </p>
+                        <p class="text m-30">
+                            <strong>Horário de funcionamento: 09h00 às 18h00</strong>
+                        </p>
+                        <a href="#" target="_blank" class="btn btn-red">Como chegar</a> 
+                    </div>
+                </div>
+                <!-- End -->
+                <!-- Partial 2 -->
+                <div class="col-md-3 col-12 m-20 p-0 todos zona-sul">
+                    <div class="visite--region">
+                        <p class="title">
+                            Zona Sul
+                        </p>
+                        <p class="text h-65">
+                            Estrada de Itapecerica, 1700 (em frente ao Hospital do Campo Limpo)
+                        </p>
+                        <p class="text m-30">
+                            <strong>Horário de funcionamento: 09h00 às 21h00</strong>
+                        </p>
+                        <a href="#" target="_blank" class="btn btn-red">Como chegar</a>
+                    </div>
+                </div>
+                <div class="col-md-3 col-12 m-20 p-0 todos zona-leste">
+                    <div class="visite--region">
+                        <p class="title">
+                            Shopping Itaquera
+                        </p>
+                        <p class="text h-65">
+                            Avenida José Pinheiro Borges, s/n Shopping Metrô Itaquera - Piso Metrô - Itaquera
+                        </p>
+                        <p class="text m-30">
+                            <strong>Horário de funcionamento: 10h00 às 22h00</strong>
+                        </p>
+                        <a href="#" target="_blank" class="btn btn-red">Como chegar</a>
+                    </div>
+                </div>
+                <div class="col-md-3 col-12 m-20 p-0 todos zona-sul">
+                    <div class="visite--region"> 
+                        <p class="title">
+                            Laguna
+                        </p>
+                        <p class="text h-65">
+                            Rua Laguna, 457 (esquina com a Rua Castro Verde)
+                        </p>
+                        <p class="text m-30">
+                            <strong>Horário de funcionamento: 09h00 às 19h00</strong>
+                        </p>
+                        <a href="#" target="_blank" class="btn btn-red">Como chegar</a> 
+                    </div>
+                </div>
+                <div class="col-md-3 col-12 m-20 p-0 todos zona-norte">
+                     <div class="visite--region"> 
+                        <p class="title">
+                            Zona Norte Rudge
+                        </p>
+                        <p class="text h-65">
+                            Avenida Rudge, 305 – Bom Retiro
+                        </p>
+                        <p class="text m-30">
+                            <strong>Horário de funcionamento: 09h00 às 20h00</strong>
+                        </p>
+                        <a href="#" target="_blank" class="btn btn-red">Como chegar</a> 
+                    </div>
+                </div>
+                <!-- END -->
+                <!-- Partial 3 -->
+                <div class="col-md-3 col-12 m-20 p-0 todos zona-sul">
+                    <div class="visite--region">
+                        <p class="title">
+                            Zona Sul
+                        </p>
+                        <p class="text h-65">
+                            Avenida Jacu Pêssego (esquina com a Rua Agrimensor Sugaya, 7)
+                        </p>
+                        <p class="text m-30">
+                            <strong>Horário de funcionamento: 09h00 às 18h00</strong>
+                        </p>
+                        <a href="#" target="_blank" class="btn btn-red">Como chegar</a>
+                    </div>
+                </div>
+                <div class="col-md-3 col-12 m-20 p-0 todos zona-sul">
+                    <div class="visite--region">
+                        <p class="title">
+                            Cupecê
+                        </p>
+                        <p class="text h-65">
+                            Avenida Cupecê, 1110 - Santo Amaro
+                        </p>
+                        <p class="text m-30">
+                            <strong>Horário de funcionamento: 09h00 às 18h00</strong>
+                        </p>
+                        <a href="#" target="_blank" class="btn btn-red">Como chegar</a>
+                    </div>
+                </div>
+                <div class="col-md-3 col-12 m-20 p-0 todos centro">
+                    <div class="visite--region"> 
+                        <p class="title">
+                            Centro Independência
+                        </p>
+                        <p class="text h-65">
+                            Rua Independência, 458 – Cambuci
+                        </p>
+                        <p class="text m-30">
+                            <strong>Horário de funcionamento: 09h00 às 20h00</strong>
+                        </p>
+                        <a href="#" target="_blank" class="btn btn-red">Como chegar</a> 
+                    </div>
+                </div>
+                <div class="col-md-3 col-12 m-20 p-0 todos jundiai">
+                     <div class="visite--region"> 
+                        <p class="title">
+                            Jundiaí
+                        </p>
+                        <p class="text h-65">
+                            Avenida Nove de Julho, 1515 - Posto Bate Bola
+                        </p>
+                        <p class="text m-30">
+                            <strong>Horário de funcionamento: 09h00 às 18h00</strong>
+                        </p>
+                        <a href="#" target="_blank" class="btn btn-red">Como chegar</a> 
+                    </div>
+                </div>
+                <!-- END -->
+            </div> 
+        </div>
+    </section>
+
+    <section class="historia" id="historia">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-12">
+                    <h3 class="historia--title">Essa história também pode ser a sua! </h3>
+                    <p class="historia--text">#queroganharumape</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 col-12">
+                    <div class="historia--video">
+                        <a href="#" class="link-video" data-video-id="X1qK3Okfn-k"> 
+                            <picture>
+                                <source class="" srcset="assets/images/play.webp" type="image/webp">
+                                <source class="" srcset="assets/images/play.png" type="image/png"> 
+                                <img class="" src="assets/images/play.png" alt="play">
+                            </picture>
+                        </a>
+                        <picture>
+                            <source class="img-fluid" srcset="assets/images/karine-jefferson.webp" type="image/webp">
+                            <source class="img-fluid" srcset="assets/images/karine-jefferson.png" type="image/png"> 
+                            <img class="img-fluid" src="assets/images/karine-jefferson.png" alt="Karine e Jefferson">
+                        </picture> 
+                        <p class="historia--nome">Karine e Jefferson</p>
+                        <hr>
+                    </div>
+                </div>
+                <div class="col-md-4 col-12">
+                    <div class="historia--video">
+                        <a href="#" class="link-video" data-video-id="aBLqQsQzJYw">
+                            <picture>
+                                <source class="" srcset="assets/images/play.webp" type="image/webp">
+                                <source class="" srcset="assets/images/play.png" type="image/png"> 
+                                <img class="" src="assets/images/play.png" alt="play">
+                            </picture>    
+                        </a>
+                        <picture>
+                            <source class="img-fluid" srcset="assets/images/lucas-debora.webp" type="image/webp">
+                            <source class="img-fluid" srcset="assets/images/lucas-debora.png" type="image/png"> 
+                            <img class="img-fluid" src="assets/images/lucas-debora.png" alt="Lucas e Débora">
+                        </picture> 
+                        <p class="historia--nome">Lucas e Débora</p>
+                        <hr>
+                    </div>
+                </div>
+                <div class="col-md-4 col-12">
+                    <div class="historia--video">
+                        <a href="#" class="link-video" data-video-id="pi6oVk0cJnw">  
+                             <picture>
+                                <source class="" srcset="assets/images/play.webp" type="image/webp">
+                                <source class="" srcset="assets/images/play.png" type="image/png"> 
+                                <img class="" src="assets/images/play.png" alt="play">
+                            </picture>  
+                        </a>
+                        <picture>
+                            <source class="img-fluid" srcset="assets/images/sabrina-samuel.webp" type="image/webp">
+                            <source class="img-fluid" srcset="assets/images/sabrina-samuel.png" type="image/png"> 
+                            <img class="img-fluid" src="assets/images/sabrina-samuel.png" alt="Sabrina e Samuel">
+                        </picture> 
+                        <p class="historia--nome">Lucas e Débora</p>
+                        <hr>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="overlay">
+            <div class="video"> 
+                <iframe width="100%" height="100%" frameborder="0" id="video" frameborder="0" allowfullscreen></iframe>
+                <a href="#" class="video--close">X</a>
             </div>
         </div>
     </section>
